305dfc0002656e2347a45b557eb031448962d980
lizhealy <lizhealy8@gmail.com>
2017-01-11 15:17:02 -0500

    prime number commit

--- Prime/ViewController.swift
+++ Prime/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
+    @IBAction func submitPressed(_ sender: Any) {
+        
+        if let isPrime = Int(number.text!) {
+            if isPrime % 2 == 0 {
+                result.text = "No"
+            }
+            else {
+                result.text = "Yes"
+            }
+        }
+        else {
+            result.text = "Must enter a valid number"
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
